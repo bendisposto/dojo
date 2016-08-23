63ac6ed3b8c57833e002f3960193348bf5743f9d
Jens Bendisposto <jens@bendisposto.de>
2016-08-23 12:04:15 +0200

    removed unused getter

--- src/main/java/tddtrainer/logic/PhaseStatus.java
+++ src/main/java/tddtrainer/logic/PhaseStatus.java
@@ -9,16 +9,19 @@ import vk.core.api.CompileError;
 import vk.core.api.TestFailure;
 
 /**
- * Contains, whether the {@link Phase} is valid for the current {@link tddtrainer.catalog.Exercise} or not, the current {@link Phase} and {@link ExecutionResult}. 
+ * Contains, whether the {@link Phase} is valid for the current
+ * {@link tddtrainer.catalog.Exercise} or not, the current {@link Phase} and
+ * {@link ExecutionResult}.
+ * 
  * @author Luisa
  *
  */
 public class PhaseStatus {
-	
+
 	private boolean valid;
 	private Phase phase;
 	private ExecutionResult executionResult;
-	
+
 	public PhaseStatus(boolean valid, ExecutionResult executionResult, Phase phase) {
 		this.valid = valid;
 		this.executionResult = executionResult;
@@ -28,46 +31,45 @@ public class PhaseStatus {
 	public boolean isValid() {
 		return valid;
 	}
-	
+
 	public Phase getPhase() {
 		return phase;
 	}
 
-	public ExecutionResult getExecutionResult() {
-		return executionResult;
-	}
-	
 	void setExecutionResult(ExecutionResult executionResult) {
 		this.executionResult = executionResult;
 	}
-	
+
 	public String getExecutionResultAsString() {
 		String executionResultAsString = "Compile Errors: ";
-		if(executionResult.getCompilerResult().hasCompileErrors()) {
+		if (executionResult.getCompilerResult().hasCompileErrors()) {
 			List<CompilationResult> compilationResultsWithErrors = new ArrayList<>();
-			for(CompilationResult cr : executionResult.getCompileErrors()) {
-				if(!cr.getCompileErrors().isEmpty()) {
+			for (CompilationResult cr : executionResult.getCompileErrors()) {
+				if (!cr.getCompileErrors().isEmpty()) {
 					compilationResultsWithErrors.add(cr);
 				}
 			}
 			executionResultAsString += compilationResultsWithErrors.size() + "\n";
-			for(CompilationResult cr : compilationResultsWithErrors) {
-				executionResultAsString += "Class: " + cr.getClassName() + ", Errors: " + cr.getCompileErrors().size() + "\n";
-				for(CompileError ce : cr.getCompileErrors()) {
-					executionResultAsString += "Line " + ce.getLineNumber() + ": " + ce.getCodeLineContainingTheError().trim() + "\n" + ce.getMessage() + "\n"; 
+			for (CompilationResult cr : compilationResultsWithErrors) {
+				executionResultAsString += "Class: " + cr.getClassName() + ", Errors: " + cr.getCompileErrors().size()
+						+ "\n";
+				for (CompileError ce : cr.getCompileErrors()) {
+					executionResultAsString += "Line " + ce.getLineNumber() + ": "
+							+ ce.getCodeLineContainingTheError().trim() + "\n" + ce.getMessage() + "\n";
 				}
 			}
-		}
-		else {
+		} else {
 			executionResultAsString += "0\nSuccessful Tests: ";
-			if(executionResult.getTestResult().getNumberOfFailedTests() != 0) {
-				executionResultAsString += executionResult.getTestResult().getNumberOfSuccessfulTests() + ", Failed Tests: " + executionResult.getTestResult().getNumberOfFailedTests() + "\n";
-				for(TestFailure tf : executionResult.getTestResult().getTestFailures()) {
-					executionResultAsString += "Class: " + tf.getTestClassName() + ", Method: " + tf.getMethodName() + "\n" + tf.getMessage() +"\n";
+			if (executionResult.getTestResult().getNumberOfFailedTests() != 0) {
+				executionResultAsString += executionResult.getTestResult().getNumberOfSuccessfulTests()
+						+ ", Failed Tests: " + executionResult.getTestResult().getNumberOfFailedTests() + "\n";
+				for (TestFailure tf : executionResult.getTestResult().getTestFailures()) {
+					executionResultAsString += "Class: " + tf.getTestClassName() + ", Method: " + tf.getMethodName()
+							+ "\n" + tf.getMessage() + "\n";
 				}
-			}
-			else {
-				executionResultAsString += executionResult.getTestResult().getNumberOfSuccessfulTests() + ", Failed Tests: 0\n";
+			} else {
+				executionResultAsString += executionResult.getTestResult().getNumberOfSuccessfulTests()
+						+ ", Failed Tests: 0\n";
 			}
 		}
 		return executionResultAsString;
